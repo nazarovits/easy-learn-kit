7b1c16e0f0f160904c05d0aaf764c25a8eac86b7
nazarovits <istvan.nazarovits@gmail.com>
2025-10-02 14:12:20 +0200

    Force dynamic AbcPage load

--- src/app/abc/page.tsx
+++ src/app/abc/page.tsx
@@ -3,10 +3,10 @@ import Abc from "./ABC";
 import { getRandomWords } from "@/components/utils/getRandomWords";
 
 import { words } from "@/sources/words";
-
 const AbcPage = () => {
   const randomWords = getRandomWords([...words], 5);
   return <Abc randomWords={randomWords} />;
 };
 
+export const dynamic = "force-dynamic";
 export default AbcPage;
